d121ce19ed62d72b5a43b06c4dc065af62584246
Evgeniy <e.vasiliev.86@mail.ru>
2023-08-17 17:16:36 +0300

    Create program

--- src/model/User.java
+++ src/model/User.java
@@ -1,2 +1,81 @@
-package model;public class User {
+package model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class User {
+    private String lastName;
+    private String firstName;
+    private String Surname;
+    private LocalDate dateOfBirth;
+    private String phoneNumber;
+    private Character gender;
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String data) throws IllegalArgumentException{
+        try {
+            LocalDate dateOfBirth = LocalDate.parse(data, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            this.dateOfBirth = dateOfBirth;
+        }
+        catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("Неверная дата рождения");
+        }
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) throws IllegalArgumentException {
+        if (phoneNumber.matches("^((8|7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$")) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            throw new IllegalArgumentException("Неверный формат номера телефона");
+        }
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public void setGender(Character gender) throws IllegalArgumentException {
+        if (gender == 'm' || gender == 'f') {
+            this.gender = gender;
+        } else {
+            throw new IllegalArgumentException("Неверный формат пола");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return lastName + " " + firstName + " " + Surname + " " + dateOfBirth + " " + phoneNumber + " " + gender;
+    }
 }
--- src/view/View.java
+++ src/view/View.java
@@ -1,2 +1,19 @@
-package view;public class View {
+package view;
+
+import java.util.Scanner;
+
+public class View {
+    public String CreateUser(){
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Введите данные в формате: Фамилия Имя Отчество Дата рождения (dd.mm.yyyy) Телефон (10) Пол (f/m): ");
+        return inp.nextLine();
+    }
+
+    public void Exception(Exception e){
+        Exception(e.getMessage());
+    }
+
+    public void Exception(String str){
+        System.out.println(str);
+    }
 }
